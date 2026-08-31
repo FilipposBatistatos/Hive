e0bf29c3c508fd90f6ae74e3ee2f3a8c490ccfbc
Philip Batistatos <54430511+FilipposBatistatos@users.noreply.github.com>
2026-08-31 16:14:42 +1000

    Can select hex

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,38 +1,42 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text)
+import Html exposing (Html, div)
 import Html.Attributes exposing (style)
 import Svg exposing (Svg, svg, polygon, g)
 import Svg.Attributes exposing (viewBox, points, fill, stroke, width, height, transform, preserveAspectRatio)
+import Svg.Events
+
 
 type alias Model =
-    { turnNumber : Int }
+    { selectedHex : Maybe ( Int, Int ) }
 
 
 init : Model
 init =
-    { turnNumber = 1 }
+    { selectedHex = Nothing }
+
 
 type Msg
-    = NoOp
+    = ClickedHex ( Int, Int )
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        NoOp ->
-            model
+        ClickedHex pos ->
+            { model | selectedHex = Just pos }
 
 
 view : Model -> Html Msg
 view model =
-    div [ style "position" "relative", style "width" "100vm", style "height" "100vh"]
-        [ boardView ]
+    div [ style "position" "relative", style "width" "100vw", style "height" "100vh" ]
+        [ boardView model ]
 
-boardView : Html Msg
-boardView = 
-    svg 
+
+boardView : Model -> Html Msg
+boardView model =
+    svg
         [ width "100%"
         , height "100%"
         , viewBox "-300 -300 600 600"
@@ -41,35 +45,49 @@ boardView =
         , style "top" "0"
         , style "left" "0"
         ]
-        (List.map renderHex boardHexes)
+        (List.map (renderHex model) boardHexes)
+
 
 boardHexes : List ( Int, Int )
-boardHexes = 
+boardHexes =
     List.concatMap
         (\q -> List.map (\r -> ( q, r )) (List.range -4 4))
         (List.range -4 4)
-        |> List.filter (\( q, r ) -> abs ( q + r ) <= 4)
+        |> List.filter (\( q, r ) -> abs (q + r) <= 4)
 
-renderHex : ( Int, Int ) -> Svg Msg
-renderHex ( q, r ) =
+
+renderHex : Model -> ( Int, Int ) -> Svg Msg
+renderHex model ( q, r ) =
     let
-        ( x, y ) = 
+        ( x, y ) =
             axialToPixel q r 40
+
+        isSelected =
+            model.selectedHex == Just ( q, r )
+
+        hexFill =
+            if isSelected then
+                "#8fe0a0"
+            else
+                "white"
     in
-        g [ transform ("translate(" ++ String.fromFloat x ++ "," ++ String.fromFloat y ++ ")") ]
+    g [ transform ("translate(" ++ String.fromFloat x ++ "," ++ String.fromFloat y ++ ")") ]
         [ polygon
             [ points (hexPoints 0 0 38)
-            , fill "white"
+            , fill hexFill
             , stroke "#ddd"
+            , Svg.Events.onClick (ClickedHex ( q, r ))
             ]
             []
         ]
 
+
 axialToPixel : Int -> Int -> Float -> ( Float, Float )
-axialToPixel q r size = 
+axialToPixel q r size =
     ( size * (sqrt 3 * toFloat q + sqrt 3 / 2 * toFloat r)
     , size * (3 / 2 * toFloat r)
     )
+
 
 hexPoints : Float -> Float -> Float -> String
 hexPoints cx cy size =
